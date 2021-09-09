e73cba1750d84d5a1c3080f75ca892d845418d10
Nikola Pavlov <nikolapavlov33@gmail.com>
2021-09-09 11:07:12 +0200

    Add paddles and a ball

---
+++ pong.py
@@ -0,0 +1,37 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong Game")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Main game loop
+while True:
+  wn.update()
